a491bc53e225223cb273e0113e7e340b8ff3fb25
Juan Diego Bustamante <Juan.Bacon.B@outlook.com>
2022-10-04 17:01:13 -0500

    making build and node app

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 // importar express, la configuracion del puerto y los enrutadores express
 import express from "express";
 import { PORT } from "./config.js";
+import {dirname, join} from 'path';
+import { fileURLToPath } from "url";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
 import cors from "cors";
 // crea una aplicacion express 
 const app = express();
+const _dirname = dirname(fileURLToPath(import.meta.url))
+
+console.log(_dirname);
 app.use(cors());
 // usa la funcion json de express para manejar los json en las respuestas del servidor 
 app.use(express.json());
@@ -13,6 +18,7 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
 // despliega la aplicacion en el puerto seleccionado
+app.use(express.static(join(_dirname, '../client/dist')))
 app.listen(PORT);
 
 console.log(`Server is listening on port: ${PORT}`);
