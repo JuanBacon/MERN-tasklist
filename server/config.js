d6fa8d8dc8f0e364e005b8890ace92332d072e63
Juan Diego Bustamante <Juan.Bacon.B@outlook.com>
2022-10-04 19:31:03 -0500

    fixed port

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 // puerto seleccionado para el backend
-var port_number = process.env.PORT || 3000;
+var port_number = process.env.PORT || 80;
 console.log(process.env);
 export const PORT = port_number;
