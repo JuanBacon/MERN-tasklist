f157357f498a40c34f1931be03d9cf62e19386b9
Juan Diego Bustamante <Juan.Bacon.B@outlook.com>
2022-10-04 19:03:53 -0500

    fixed port

--- client/src/API/task.api.js
+++ client/src/API/task.api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
+const port = 5000;
 
 export const getTaskRequest = async (id) => {
-    return await axios.get("http://localhost:4000/api/tasks/" + id);
+    return await axios.get("http://localhost:"+port+"/api/tasks/" + id);
 }
 export const getTasksRequest = async () => {
-    return await axios.get("http://localhost:4000/api/tasks");
+    return await axios.get("http://localhost:"+port+"/api/tasks");
 };
 export const createTaskRequest = async (task) => {
-    return await axios.post("http://localhost:4000/api/tasks", task);
+    return await axios.post("http://localhost:"+port+"/api/tasks", task);
 };
 export const deleteTaskRequest = async (id) => {
-    return await axios.delete("http://localhost:4000/api/tasks/" + id );
+    return await axios.delete("http://localhost:"+port+"/api/tasks/" + id );
 }
 export const updateTaskRequest = async (id, newFields) => {
-    return await axios.put("http://localhost:4000/api/tasks/" + id,  newFields);
+    return await axios.put("http://localhost:"+port+"/api/tasks/" + id,  newFields);
 }
--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 // puerto seleccionado para el backend
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = process.env.PORT || 3000;
 export const PORT = port_number;
