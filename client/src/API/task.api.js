c2254390cfdd100aca5c2ca5cb3f863fb5970afb
Juan Diego Bustamante <Juan.Bacon.B@outlook.com>
2022-10-04 21:19:46 -0500

    deploying to heroku

--- client/src/API/task.api.js
+++ client/src/API/task.api.js
@@ -2,17 +2,17 @@ import axios from "axios";
 const port = 3000;
 
 export const getTaskRequest = async (id) => {
-    return await axios.get("http://localhost:"+port+"/api/tasks/" + id);
+    return await axios.get("https://juanbacon-todotasks.herokuapp.com/api/tasks/" + id);
 }
 export const getTasksRequest = async () => {
-    return await axios.get("http://localhost:"+port+"/api/tasks");
+    return await axios.get("https://juanbacon-todotasks.herokuapp.com/api/tasks");
 };
 export const createTaskRequest = async (task) => {
-    return await axios.post("http://localhost:"+port+"/api/tasks", task);
+    return await axios.post("https://juanbacon-todotasks.herokuapp.com/api/tasks", task);
 };
 export const deleteTaskRequest = async (id) => {
-    return await axios.delete("http://localhost:"+port+"/api/tasks/" + id );
+    return await axios.delete("https://juanbacon-todotasks.herokuapp.com/api/tasks/" + id );
 }
 export const updateTaskRequest = async (id, newFields) => {
-    return await axios.put("http://localhost:"+port+"/api/tasks/" + id,  newFields);
+    return await axios.put("https://juanbacon-todotasks.herokuapp.com/api/tasks/" + id,  newFields);
 }
--- server/config.js
+++ server/config.js
@@ -1,4 +1,3 @@
 // puerto seleccionado para el backend
 var port_number = process.env.PORT || 80;
-console.log(process.env);
 export const PORT = port_number;
